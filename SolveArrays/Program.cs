7023b5ea57dce170befc901118b8083f37f17626
Chip <chip.elmer@gmail.com>
2019-01-23 08:37:34 -0600

    Add project files.

---
+++ SolveArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolveArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
